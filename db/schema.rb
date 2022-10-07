a6fbd0af2098c0e31015d7d746c661fdb4665b17
Roman Cabalum <roman.cabalum@gmail.com>
2022-10-07 20:07:30 +0800

    scaffold test

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_07_113643) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_07_120420) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "adminpack"
   enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tests", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
